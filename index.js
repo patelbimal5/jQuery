8a7efdbe824f5845015f38903003882b24f98e91
patelbimal5 <68713222+patelbimal5@users.noreply.github.com>
2022-03-05 21:10:31 -0500

    adding img tag and anchor tag to play with its attributes

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 // jQuery("h1").css("color","red");
 
 $("h1").addClass("big-title margin-50"); //two css implemented in one line //addClass and removeClass
+
+
+//manupulating text
+$("h1").text("Bye")  // this will change text of h1 which is "Hello" to "Bye"
+
+$("button").html("<em>Hey<em>") // this include html tag inside button <em> to italic and <p> for para
+
+
+//manupulating attributes
