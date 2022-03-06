d7d3fc2b59ea659d40072db65cb4cd444b8476b7
patelbimal5 <68713222+patelbimal5@users.noreply.github.com>
2022-03-05 21:00:16 -0500

    adding different types of jquery syntax in comments.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-$("h1").css("color","blue");
+//$("h1").css("color","blue");
 //OR
 // jQuery("h1").css("color","red");
+
+$("h1").addClass("big-title margin-50"); //two css implemented in one line //addClass and removeClass
